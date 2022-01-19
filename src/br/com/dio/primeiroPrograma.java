e30994664f0d803e2d5940d6d8a03f369b144402
Crysmar1 <crysmar1@outlook.com>
2022-01-19 11:05:50 -0300

    Commit IntelliJ

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Olá meus amiguinhos. A soma de " + a + " mais " + b + " é igual a " + (a+b));*/
+    }
+}
+
